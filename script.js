f91ee6d122fb024d2b567299c5f5efd26acb1d2d
205737 <donalog2018@gmail.com>
2025-03-17 12:06:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+window.onload = function () {
+    if (window.innerHeight > 1080 || window.innerWidth > 1920) {
+        document.getElementById("popup").style.display = "block";
+        document.getElementById("overlay").style.display = "block";
+    }
+};
 
+function handleResponse(response) {
+    console.log("User response:", response);
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
